ac74f64cca0e34b2b4f46f9db621cb6e7e154c27
Myzel394 <50424412+Myzel394@users.noreply.github.com>
2021-03-06 13:26:54 +0100

    spelling corrected

--- constants.py
+++ constants.py
@@ -13,7 +13,7 @@ PLAYLIST_ID = "UUkZ3fSYruC0IXv6p34BHciQ"  # Uploads from "The Morpheus Vlogs"
 FAKE_NAMES = [
     "the morpheus vlogs", "the morpheus-vlogs", "morpheus vlogs", "morpheus-vlogs", "morpheus", "the morpheus"
 ]
-NOTIFY_MESSAGE = "ACHTUNG! Dies ist ein Fake-Account! Bitte meldet den Fake \"The Morpheus Vlogs\"-Kanal!"
+NOTIFY_MESSAGE = "ACHTUNG! Dies ist ein Fake-Account! Bitte meldet den fake \"The Morpheus Vlogs\"-Kanal!"
 SEARCH_AMOUNT = 900
 API_KEY = os.getenv("API_KEY")
 CLIENT_SECRET = os.getenv("CLIENT_SECRET")
